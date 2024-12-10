ddab37fe664285ac614e11178c684847c0ed5e72
y-hiroki-radiotech <bnndd439@ybb.ne.jp>
2024-12-10 23:31:45 +0900

    プロンプトの定義を整理し、タスク分類関数にドキュメンテーションを追加

--- task_classification.py
+++ task_classification.py
@@ -1,6 +1,18 @@
 from transformers import pipeline
 
 def task_classification(dataset):
+    """
+    指定されたデータセットに対してタスク分類を行う関数。
+
+    この関数は、指定されたデータセットの各入力に対してBERTモデルを使用して分類を行い、
+    予測されたラベルをデータセットに追加します。
+
+    Args:
+        dataset (pd.DataFrame): 'input'列を持つデータフレーム。各行は分類対象のテキストデータを含む。
+
+    Returns:
+        pd.DataFrame: 予測されたラベルが追加されたデータフレーム。
+    """
     model_name = "hiroki-rad/bert-base-classification-ft"
     classify_pipe = pipeline(model=model_name, device="cuda:0")
 
@@ -9,7 +21,7 @@ def task_classification(dataset):
         # モデルの予測結果を取得
         model_prediction = classify_pipe(example.input)[0]
         # 正解のラベルIDをラベル名に変換
-        results.append( model_prediction["label"])
+        results.append(model_prediction["label"])
 
     dataset["label"] = results
 
