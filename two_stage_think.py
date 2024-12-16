fc85dc0346e7377da22cebee3fd6c5840b1075b3
y-hiroki-radiotech <73370348+y-hiroki-radiotech@users.noreply.github.com>
2024-12-17 01:25:13 +0900

    Update two_stage_think.py
    change first think

--- two_stage_think.py
+++ two_stage_think.py
@@ -12,7 +12,7 @@ class TwoStageThinking:
         self.llm = llm
         self.sampling_params = SamplingParams(
             repetition_penalty=1.1,
-            max_tokens=1024
+            max_tokens=1028
         )
 
     def _generate_text(self, prompt):
@@ -33,7 +33,7 @@ class TwoStageThinking:
             print(f"テキスト生成エラー: {str(e)}")
             return ""
 
-    def first_thinking(self, data):
+    def first_thinking(self, data, few_shot_example):
         """
         一段階目の思考生成
         
@@ -43,10 +43,14 @@ class TwoStageThinking:
         Returns:
             str: 生成された回答
         """
-        prompt = f"""## 指示:あなたは優秀な日本人の問題解決のエキスパートです。以下のステップで質問に取り組んでください：\n\n1. 質問の種類を特定する（事実確認/推論/創造的回答/計算など）\n2. 重要な情報や制約条件を抽出する\n3. 解決に必要なステップを明確にする\n4. 回答を組み立てる
-        質問をよく読んで、じっくり冷静に考え、考えをステップバイステップでまとめましょう。適切な回答を簡潔に出力してください。
+        prompt = f"""## 指示:あなたは優秀な日本人の問題解決のエキスパートです。以下のステップで質問に取り組んでください：\n\n1. 質問の種類を特定する（事実確認/推論/創造的回答/計算など）\n2. 重要な情報や制約条件を抽出する\n3. 解決に必要なステップを明確にする\n4. 回答を組み立てる\n\n
+        回答例は以下のものを参考にできます: {few_shot_example}\n\n
+        質問をよく読んで、じっくり冷静に考え、考えをステップバイステップでまとめましょう。
+        回答を作成する前に深呼吸して、分析結果を踏まえて解決しているかよく考えましょう。
+        適切な回答を簡潔に出力してください。
 
-        質問:{data.input}\n回答:"""
+        質問:{data.input}\n回答: """
+        
         
         return self._generate_text(prompt)
 
@@ -63,12 +67,12 @@ class TwoStageThinking:
         """
         prompt = f"""## 指示:これは一度あなたが考えた回答です。\n#あなたの回答:{first_output}
 
-        もう一度質問を見てみましょう。\n質問:{data.input}
-        あなたの回答は質問に対して十分に答えているか、よく考えてください。文章は日本語で表現してください。回答は簡潔にしてください。"""
+        日本語で簡潔に答えられているかチェックして、必要であれば修正してください。
+        回答:　"""
         
         return self._generate_text(prompt)
 
-    def generate_complete_response(self, data):
+    def generate_complete_response(self, data, few_shot_example):
         """
         完全な二段階思考プロセスを実行
         
@@ -78,7 +82,8 @@ class TwoStageThinking:
         Returns:
             tuple: (一段階目の回答, 二段階目の回答)
         """
-        first_result = self.first_thinking(data)
-        time.sleep(1)
-        second_result = self.second_thinking(data, first_result)
-        return first_result, second_result
+        first_result = self.first_thinking(data,  few_shot_example.split("->")[1])
+        time.sleep(2)
+        # second_result = self.second_thinking(data, first_result)
+        # time.sleep(2)
+        return first_result
